8ffd390c662d1a6af3b43191545ce7d76ce991d7
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2015-11-02 14:22:46 -0500

    remove solution

--- lib/student.rb
+++ lib/student.rb
@@ -1,16 +1,3 @@
-class Student < User
+class Student 
 
-  attr_accessor :knowledge
-
-  def initialize
-    @knowledge = []
-  end
-
-  def learn(knowledge)
-    @knowledge << knowledge
-  end
-
-  def knowledge
-    @knowledge
-  end
 end
--- lib/user.rb
+++
@@ -1,5 +0,0 @@
-class User
-
-  attr_accessor :first_name, :last_name
-
-end
