dd32ff500ce991046ee9ae689a1a13524a406cea
Daryl Rogony <darylrogony@gmail.com>
2022-06-12 11:09:24 +0300

    commit tree 53d3e6c983d508664c111bd8beed56f1aae4a467

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
 
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(info)
+    @knowledge << info
+  end
+ 
 end
---
+++ lib/user.rb
@@ -0,0 +1,15 @@
+class User
+  
+  attr_accessor :first_name, :last_name
+
+
+  # def username(firstname = "John", lastname = "Doe") 
+  #   @firstname = firstname
+  #   @lastname = lastname
+  # end
+
+  # def name
+  #   @firstname
+  #   @lastname
+  # end
+end
