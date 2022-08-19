5a8e83cba09b2b432be9d8031a1434dd01c270b8
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-08-19 12:44:11 +0300

    commit tree aaff8636c04710f06d2b1d181408a66aeb527dd4

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
+class Student < User
+
+      attr_reader :knowledge
+      def initialize
+        @knowledge =[]
+      end
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
 
 end
