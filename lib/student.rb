93a584d560c2b3b934166a3556bfd918fdb76618
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-10-23 16:50:40 +0300

    commit tree a02afc4af44d129b18bdd641137383b338a2676a

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+# require_relative './user'
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize
+        @knowledge =[]
+    end
+    # Student#learn takes in an argument of a string of knowledge and adds it to the student's knowledge array
+    def learn(knowledge)
+        @knowledge << knowledge
+    end
 end
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,10 @@
-class Teacher
+# require_relative './user'
+class Teacher < User
 
   KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-  
+  def teach
+    # You might want to look up the Array#sample method
+    KNOWLEDGE.sample
+  end
 end
